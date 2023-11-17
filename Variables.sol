000b195dadcdbdd81dbadaff746e04858b3d0a1d
Sonu Kumar Yadav <yadavsonukumar036@gmail.com>
2023-11-17 15:01:25 +0530

    Variables in SOlidity

---
+++ Variables.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.22;
+contract Hello {
+    string public welcome ="Hello World";
+    bool isReady=true;
+    uint8 MAX_SUPPLY=10;
+    uint256 WAIT_TIME=1 days;
+
+    //There are 8 16 128 256 uint
+}
